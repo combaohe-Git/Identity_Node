a722dab64963d271381f3bfc43686024fe00306f
niudada0623 <niudada0623@gmail.com>
2026-03-08 16:13:40 +0000

    ATO Protocol 176 - TBB Minting v9.2

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,16 @@
+const { ethers } = require("hardhat");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
 
-  const Token = await ethers.getContractFactory("Protocol176");
-  const token = await Token.deploy({ gasLimit: 3000000 });
+  const Protocol176 = await ethers.getContractFactory("Protocol176");
+  const protocol176 = await Protocol176.deploy();
 
-  await token.waitForDeployment();
+  await protocol176.deployed();
 
-  console.log("Token address:", await token.getAddress());
+  console.log("Protocol176 deployed to:", protocol176.address);
 }
 
 main()
